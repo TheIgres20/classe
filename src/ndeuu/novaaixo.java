4973f3b0522084e946c1f9c1988548380833ee33
Sergi Tomàs <Sergi Toms@192.168.1.36>
2018-02-26 23:16:50 +0100

    hola que pasa nenn

---
+++ src/ndeuu/novaaixo.java
@@ -0,0 +1,10 @@
+package ndeuu;
+
+public class novaaixo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ndeuu/ondia.java
@@ -0,0 +1,5 @@
+package ndeuu;
+
+public class ondia {
+
+}
